405a61fbb96fd371d88fc09e398fd04b057f8444
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-02-07 15:07:33 +0000

    created a list with directives

--- angular-project/src/app/courses.component.ts
+++ angular-project/src/app/courses.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+                <h2>{{ title }}</h2>
+                <ul>
+                    <li *ngFor="let course of courses">
+                        {{ course }}
+                    </li>
+                </ul>
+              `
 })//decorator function
 export class CoursesComponent{
+    title = "List of courses";
+    courses = ["course1","course2","course3"]; //*objects in real app
     
 }

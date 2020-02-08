b37894c29828b7093026799f4bd1f1c82eacdde5
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-02-08 15:01:42 +0000

    added authors service

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthorsService } from './authors.service';
 import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -6,19 +7,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    CoursesService
+    CoursesService,
+    AuthorsService
   ],
   bootstrap: [AppComponent]
 })
--- angular-project/src/app/courses.component.ts
+++ angular-project/src/app/courses.component.ts
@@ -1,23 +1,21 @@
-import { CoursesService } from './courses.service';
+//import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
     template: `
-                <h2>{{ title }}</h2>
-                <ul>
-                    <li *ngFor="let course of courses">
-                        {{ course }}
-                    </li>
-                </ul>
+                <img [src]="imageUrl" />
+                <table>
+                    <tr>
+                        <td [attr.colspan]="colSpan"></td>//targeting the colSpan attribute of the html element
+                    </tr>
+                </table>
               `
 })//decorator function
 export class CoursesComponent{
-    title = "List of courses";
-    courses;
+
+    imageUrl = "http://lorempixel.com/400/200";
+    colSpan = 2;
     
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
-    }
     
 }

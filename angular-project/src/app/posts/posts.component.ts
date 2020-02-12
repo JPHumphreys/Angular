66b9d3dbe916507a5bd6ee159e48ea668d3a265c
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-02-12 11:37:16 +0000

    added update,delete and ngOnInit to the posts componenet

--- angular-project/src/app/posts/posts.component.ts
+++ angular-project/src/app/posts/posts.component.ts
@@ -6,15 +6,49 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent  {
+export class PostsComponent implements OnInit {
   posts: any;
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: HttpClient){
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  constructor(private http: HttpClient){
+    
+  }
+  
+  ngOnInit(){
+    this.http.get(this.url)
     .subscribe(Response =>{
       this.posts = Response;
       //console.log(this.posts);
-    }) //similar to onload - when ready we will be notified
+    }); //similar to onload - when ready we will be notified
+  }
+
+  createPost(input: HTMLInputElement){
+    let post = {
+      title: input.value
+    };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+    .subscribe(Response => {
+      post['id'] = Response['id'];
+      this.posts.splice(0,0,post);
+      console.log(this.posts);
+    });
+  }
+
+  updatePost(post){
+
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+    .subscribe(Response => {
+      console.log(Response);
+    });
+  }
+
+  deletePost(post){
+    this.http.delete(this.url + '/' + post.id)
+    .subscribe(Response => {
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index, 1);
+    });
   }
 
 }

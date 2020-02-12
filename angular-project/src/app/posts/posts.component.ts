e19863c0ae79d10d9f28d6d9ed958410141d2e5f
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-02-12 14:00:09 +0000

    added unexpected errors to the CRUD subscribes

--- angular-project/src/app/posts/posts.component.ts
+++ angular-project/src/app/posts/posts.component.ts
@@ -15,9 +15,11 @@ export class PostsComponent implements OnInit {
   
   ngOnInit(){
     this.service.getPost()
-    .subscribe(Response =>{
+    .subscribe(
+      Response =>{
       this.posts = Response;
-    },error => {
+    },
+    error => {
       alert('An unexpected error occured.');//use toasts instead
       console.log(error);
     }); 
@@ -29,25 +31,40 @@ export class PostsComponent implements OnInit {
     };
     input.value = '';
     this.service.createPost(post)
-    .subscribe(Response => {
+    .subscribe(
+      Response => {
       post['id'] = Response['id'];
       this.posts.splice(0,0,post);
       console.log(this.posts);
+    },
+    error => {
+      alert('An unexpected error has occured.');
+      console.log(error);
     });
   }
 
   updatePost(post){
     this.service.updatePost(post)
-    .subscribe(Response => {
+    .subscribe(
+      Response => {
       console.log(Response);
+    },
+    error => {
+      alert('An unexpected error has occured.');
+      console.log(error);
     });
   }
 
   deletePost(post){
     this.service.deletePost(post.id)
-    .subscribe(Response => {
+    .subscribe(
+      Response => {
       let index = this.posts.indexOf(post);
       this.posts.splice(index, 1);
+    },
+    error => {
+      alert('An unexpected error has occured.');
+      console.log(error);
     });
   }
 
